f8e4f94e101ec901e953d8fca1a26157da6e68de
Leon Si <leonzalion@gmail.com>
2021-06-20 13:22:58 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-base"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+client.on("ready", () => {
+  console.log("Bot is ready.");
+});
+
+client.on("message", (msg) => {
+  if (msg.author.bot === true) return;
+});
+
 client.login(process.env.BOT_TOKEN).catch((err) => {
   console.error(`Error: ${err}`);
 });
